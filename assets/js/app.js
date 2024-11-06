2346ba0caa0713053983b44e64129964f008041d
rasadlli <resadesedli26@gmail.com>
2024-11-07 00:07:31 +0400

    tests

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,23 @@
 const personID = "1062193446&quot"
 function test() {
-    $.get(`https://api.linkedin.com/v2/people/(id:${personID})`, function (data, status) {
-        console.log("Data: " + data + "\nStatus: " + status);
-    });
+    const url = "https://api.lix-it.com/v1/person?profile_link=https://linkedin.com/in/alfie-lambert";
+    const lixApiKey = "7qWq0z7MfPLOfHQCI02ihqrPKg5rhAdL6L3ucm26OFCCgL0PN76EIVCr63cE"
+    
+    const headers = {
+        'Authorization': lixApiKey, // replace lixApiKey with your actual API key variable or value
+    };
+    
+    fetch(url, {
+        method: "GET",
+        headers: headers
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Network response was not ok " + response.statusText);
+            }
+            return response.json(); // if the API returns JSON
+        })
+        .then(data => console.log(data))
+        .catch(error => console.error("There was a problem with the fetch operation:", error));
+    
 }
